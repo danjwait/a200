8ee2fb3e91d4584c01ad9ff2bf51942787ee1fbc
Dan Wait <djwait@calpoly.edu>
2021-01-24 21:05:25 -0800

    Update to USNO algorithms

--- raDecPointing.py
+++ raDecPointing.py
@@ -3,7 +3,14 @@
 Olivia Burd, Dan Wait
 Cal Poly Aerospace Engineering
 
+Mk I: (present version)
+runs as a stand-alone tool with hard-coded inputs 
+
+Mk II: (planned version)
+runs as a callable module within miniHubble python instance
+
 Args:
+    (For Mk II Version)
     Input target dictionary of RA (hhmmss) and Dec (ddmmss)
     Input observer dictionary of lat (deg_fract) and lon (deg_fract)
     Input observation time (local 24 hour, HH,MM,SS)
@@ -16,46 +23,50 @@ Raises:
 """
 
 ## imports
-import math # for the geometry
+import math # for trigonometric functions
 import datetime # for date and time conversions
 import platform # for determining the platform OS
 import os # for commands to the OS
 
-## Clear the output
+## MK I: Clear the output
 platformName = platform.system()
 if platformName  == 'Windows':
     os.system('cls')
 else:
     os.system('clear')
 
-## Constants & Parameters
-earth_siderealDay_sec = 86164.0905 
-#  23 h 56 min 4.0905 s per https://en.wikipedia.org/wiki/Sidereal_time
-earth_siderealRate_degSec = 360/earth_siderealDay_sec
+### Constants & Parameters ###
 
-# J2000 epoch:  1 January 2000, 12:00 UTC 
+# J2000 epoch: 1 January 2000, 12:00 UTC 
 jd_J2000 = 2451545.0
 # take off the 12:00 hours from J2000 epoch for nutation and precession calculations
 jd2000 = jd_J2000 - 0.5
 julainYear_ddFract = 365.35
+
+# Parameters for solving for Greenwich Apparent Sidereal Time (GAST)
+# Per archived version of USNO:
+# https://web.archive.org/web/20190524114447/https://aa.usno.navy.mil/faq/docs/GAST.php
+
+# TODO update:
 
 # nutation patameters
 L0_deg = 99.967794687
 L1_deg = 360.98564736628603
 L2_deg = 2.907879*10**-13
 L3_deg = -5.302*10**-22
+
 # equation of eqninox parameters
 M0_deg = 15.04106864026192
 M1_deg = 2.423233*10**-14
 M2_deg = -6.628*10**-23
 
-##  Input observation values values
+###  Input observation values values ###
 
 # Target dictionary 
 
 # Betelgeuse 
-# Right ascension	05h 55m 10.30536s
-# Declination	+07° 24′ 25.4304″
+# Right ascension 5h 55m 10.30536s
+# Declination +07° 24′ 25.4304″
 # Per https://en.wikipedia.org/wiki/Betelgeuse
 
 target = {'name':'Betelgeuse','RA_hhmmss': [5,55,10.30536], 'dec_ddmmss': [7, 24, 25.4304 ]}
@@ -75,6 +86,7 @@ observer['lat_rad'] = math.radians(observer['lat_degFract'])
 observer['lon_rad'] = math.radians(observer['lon_degFract']) 
 
 # Observation time - local, 24 hour clock
+# TODO define this better, in particular the time format (UTC, UT, GPS?)
 obsDateTime ={'yyyy': 2021, 'mm': 4, 'date': 1, 'hh': 22, 'mm':00, 'ss':00}
 
 # day_of_year = datetime.datetime.utcnow()
@@ -85,7 +97,7 @@ ic = date.isocalendar()
 # convert today to julian date
 jdNow = jd2000 + (ic[0]-2000)*julainYear_ddFract + ic[1]*7 + ic[2]
 
-## Solve for Azimuth and Elevation in NED for Target at Observer 
+### Solve for Azimuth and Elevation in NED for Target at Observer ###
 
 # Solve Local Mean Sidereal Time (LMST)
 
@@ -97,9 +109,9 @@ thetaZero_deg = L0_deg + L1_deg*jdNow + thetaP_deg
 
 localHourAngle_rad = math.pi/8 # just fake a number for now
 
-# Transformation from equitorial to Az/El
-# per "Observational Astronomy Techniques and Instrumentation" E. Sutton
-# Positional astronomy 3.3.5
+# Transformation from Celestial Equitorial to Az/El
+# Per archived version of USNO:
+# https://web.archive.org/web/20181110011637/http://aa.usno.navy.mil/faq/docs/Alt_Az.php
 
 # shorthand variable names
 dec_rad = target['dec_rad']
@@ -107,14 +119,15 @@ lat_rad = observer['lat_rad']
 h_rad = localHourAngle_rad
 
 # solve for azimuth in radians
-az_rad = math.asin(math.sin(dec_rad)*math.sin(lat_rad) + math.cos(dec_rad)*math.cos(h_rad)*math.cos(lat_rad))
+az_rad = math.asin(math.cos(h_rad)*math.cos(dec_rad)*math.cos(lat_rad) + math.sin(dec_rad)*math.sin(lat_rad))
 az_deg = math.degrees(az_rad)
 
 # solve for elevation in radians
-el_rad = math.asin((-1*math.cos(dec_rad)*math.sin(h_rad))/(math.cos(az_rad)))
+el_rad = math.atan( (-1*math.sin(h_rad)) / (math.tan(dec_rad)*math.cos(lat_rad) - math.sin(lat_rad)*math.cos(h_rad)) )
+# el_rad = math.asin((-1*math.cos(dec_rad)*math.sin(h_rad))/(math.cos(az_rad))) # old version
 el_deg = math.degrees(el_rad)
 
-## Display Outputs
+### Mk I: Display Outputs ###
 print('At ' + observer['observatory'] + ' the target ' + target['name'] + ' will be positioned at:')
 print(f'azimuth: {az_deg:.3f} degrees')
 print(f'elevation: {el_deg:.3f} degrees')
