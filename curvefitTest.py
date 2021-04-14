7a6f95220c291a40eb8cd202328696cea0a20818
Dan Wait <djwait@calpoly.edu>
2021-04-13 18:42:07 -0700

    Trying to fix polyfit

--- curvefitTest.py
+++ curvefitTest.py
@@ -24,9 +24,7 @@ Raises:
 
 ### imports ###
 import numpy as np
-import scipy as sp
-#import math # for trigonometric functions
-#import datetime # for date and time conversions
+from scipy import optimize
 import platform # for determining the platform OS
 import os # for commands to the OS
 
@@ -36,6 +34,16 @@ if platformName  == 'Windows':
     os.system('cls')
 else:
     os.system('clear')
+
+### Definitions ###
+
+# 4th order polynomial definition for curve fit
+def poly_func(x, C4, C3, C2, C1, C0):
+    return C4*(x**4.) + C3*(x**3.) + C2*(x**2.) + C1*x + C0
+
+# sine and constant definition for curve fit
+def sin_func(x, A, B, C):
+    return A * np.sin(B * x) + C
 
 ### Constants & Parameters ###
 # Per JPL HORIZONS data files:
@@ -69,7 +77,7 @@ marsEphemDict = {
 # remove file header and footer, to file of just rows of vectors
 # read in the data file of ephemieris vectors for generating as numpy array here
 # assume JPL HORIZONS Type 1 vector file of JD, X, Y, Z where X,Y,Z are in AU
-marsEphemDict["emphemerisArray"] = np.loadtxt("./marsShorter.txt", delimiter = ',',usecols = (0,2,3,4))
+marsEphemDict["emphemerisArray"] = np.loadtxt("./marsShorter.txt", delimiter=',', usecols=(0,2,3,4), converters={1: str})
 
 # setup for curve fitting by figuring out the time span of data in the file
 # start of file time span
@@ -84,43 +92,100 @@ marsEphemDict["timeSpan"] = marsEphemDict["endJD"]- marsEphemDict["startJD"]
 # check to see how the time span of the vector file compares to the orbtial period
 marsEphemDict["orbitalPeriodFraction"] = marsEphemDict["timeSpan"]/marsEphemDict["orbitalPeriod_days"]
 
+#print(marsEphemDict["emphemerisArray"][:,0])
+
 # create a set of X, Y, Z curve fit equations based on how much of an orbit is in the file
 if marsEphemDict["orbitalPeriodFraction"] < 0.5:
     # file is less than 1/2 orbital period use a poly fit
     marsEphemDict["curveFitType"] = "poly"
 
-    def poly_func(x, C4, C3, C2, C1, C0):
-        return C4 * x**4 + C3*x**3 + C2*x**2 + C1*x + C0
-    Xparams, Xparams_covariance = sp.optimize.curve_fit(poly_func, 
-        marsEphemDict["emphemerisArray"][0,:], 
-        marsEphemDict["emphemerisArray"][2,:],
-        p0=[2, 2])
     # X position equation as a function of JD, 4th order polyfit
+    marsEphemDict["Xparams"], marsEphemDict["Xparams_cov"] = optimize.curve_fit(
+        poly_func,
+        marsEphemDict["emphemerisArray"][:,0], 
+        marsEphemDict["emphemerisArray"][:,1])
+
     # Y position equation as a function of JD, 4th order polyfit
-    # X position equation as a function of JD, 4th order polyfit
-    pass
+    marsEphemDict["Yparams"], marsEphemDict["Yparams_cov"] = optimize.curve_fit(
+        poly_func,
+        marsEphemDict["emphemerisArray"][:,0], 
+        marsEphemDict["emphemerisArray"][:,2])
+
+    # Z position equation as a function of JD, 4th order polyfit
+    marsEphemDict["Zparams"], marsEphemDict["Zparams_cov"] = optimize.curve_fit(
+        poly_func,
+        marsEphemDict["emphemerisArray"][:,0], 
+        marsEphemDict["emphemerisArray"][:,3])
+
 else:
     # file has more than 1/2 on orbital period, use sine fit
     marsEphemDict["curveFitType"] = "sine"
 
-    def sin_func(x, a, b):
-        return a * np.sin(b * x)
-    params, params_covariance = sp.optimize.curve_fit(
-        sin_func, 
-        marsEphemDict["emphemerisArray"][0,:], 
-        marsEphemDict["emphemerisArray"][2,:],
-        p0=[2, 2])
+    pass # pass for now
 
     # X position equation as a function of JD, sine function fit
+    marsEphemDict["Xparams"], marsEphemDict["Xparams_cov"] = optimize.curve_fit(
+        sin_func,
+        marsEphemDict["emphemerisArray"][:,0], 
+        marsEphemDict["emphemerisArray"][:,1])
+
     # Y position equation as a function of JD, sine function fit
+    marsEphemDict["Yparams"], marsEphemDict["Yparams_cov"] = optimize.curve_fit(
+        sin_func,
+        marsEphemDict["emphemerisArray"][:,0], 
+        marsEphemDict["emphemerisArray"][:,2])
+
     # Z position equation as a function of JD, sine function fit
-    pass
+    marsEphemDict["Zparams"], marsEphemDict["Zparams_cov"] = optimize.curve_fit(
+        sin_func,
+        marsEphemDict["emphemerisArray"][:,0], 
+        marsEphemDict["emphemerisArray"][:,3])
     
 # TODO complete the vector tree for all the ephemeris files
 
 # TODO for Mk II, need to pass the ephemeris dictionaries w/ curve fit terms to miniHubble somehow
 
 # TODO For Mk I, solve the vector tree for the X, Y, Z vector from miniHubble to target at time
+# Test julian date
+testJD = marsEphemDict["startJD"] + 42.42
+
+# TODO if input date is outside of range, should catch error
+
+# based on type of fit, pull out parameters and solve for X, Y, and Z at time
+if marsEphemDict["curveFitType"] == "poly":
+    C4 = marsEphemDict["Xparams"][0]
+    C3 = marsEphemDict["Xparams"][1]
+    C2 = marsEphemDict["Xparams"][2]
+    C1 = marsEphemDict["Xparams"][3]
+    C0 = marsEphemDict["Xparams"][4]
+    testX = poly_func(testJD, C4, C3, C2, C1, C0)
+
+    C4 = marsEphemDict["Yparams"][0]
+    C3 = marsEphemDict["Yparams"][1]
+    C2 = marsEphemDict["Yparams"][2]
+    C1 = marsEphemDict["Yparams"][3]
+    C0 = marsEphemDict["Yparams"][4]
+    testY = poly_func(testJD, C4, C3, C2, C1, C0)
+
+    C4 = marsEphemDict["Zparams"][0]
+    C3 = marsEphemDict["Zparams"][1]
+    C2 = marsEphemDict["Zparams"][2]
+    C1 = marsEphemDict["Zparams"][3]
+    C0 = marsEphemDict["Zparams"][4]
+    testZ = poly_func(testJD, C4, C3, C2, C1, C0)
+
+elif marsEphemDict["curveFitType"] == "sine":
+    pass # pass for now
+    testX = sin_func(x, A, B, C)
+
+else:
+    pass # pass for now
+
+print("X coefficents are: ", marsEphemDict["Xparams"])
+print("test julian date is: ", testJD)
+print("X position values at test JD is (in AU): ", testX)
+print("Y position values at test JD is (in AU): ", testY)
+print("Z position values at test JD is (in AU): ", testZ)
 
 # TODO Convert X, Y, Z vectors to RA/Dec, then feen to Olivia's RA/Dec to Az/El solver
 
