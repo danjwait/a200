29dfef5ac6c2e8647dbb9fe1d57d5d9cd9bda9ff
Dan Wait <djwait@calpoly.edu>
2021-05-25 22:52:22 -0700

    added projections

--- Vect_2_RA_Dec.py
+++ Vect_2_RA_Dec.py
@@ -7,6 +7,18 @@ import numpy as np # install numpy
 # Hippocamp 
 # Flora
 
+# DJW - I think the XYZ vector are in Earth orbit plane, not Earth equitorial plane
+#  Reference epoch: J2000.0
+# X-Y plane: adopted Earth orbital plane at the reference epoch
+# Note: obliquity of 84381.448 arcseconds (IAU76) wrt ICRF equator
+# X-axis   : ICRF
+# Z-axis   : perpendicular to the X-Y plane in the directional (+ or -) sense
+# of Earth's north pole at the reference epoch.
+
+# use obliquity to help convert XYZ vectors to I J K components
+obliquity_deg = (84381.448 / 3600)
+obliquity_rad = np.radians(obliquity_deg)
+
 ## Designates i, j, k vectors from target to Earth Vector: 
 
 # Flora 
@@ -14,69 +26,85 @@ FloraEphemDict = {
     'head': 'Flora', # from header 
     'tail': 'Earth', # from header 
 }
-FloraEphemDict["ephemerisArray_FloraEarth"] = np.loadtxt("./Horizons New files/Flora_Earth.txt", delimiter=',',usecols=(0,2,3,4))
+FloraEphemDict["ephemerisArray_FloraEarth"] = np.loadtxt("./Flora_Earth.txt", delimiter=',',usecols=(0,2,3,4))
 # pull JD vectors 
 FloraEarth_JD = FloraEphemDict["ephemerisArray_FloraEarth"][:,0]
-# pull out i, j, k
+# pull out i, j, k ; I is direct from X, but project Y and Z with obliquity to get J and K
 FloraEarth_i = FloraEphemDict["ephemerisArray_FloraEarth"][:,1]
-FloraEarth_j = FloraEphemDict["ephemerisArray_FloraEarth"][:,2]
-FloraEarth_k = FloraEphemDict["ephemerisArray_FloraEarth"][:,3]
+FloraEarth_j = FloraEphemDict["ephemerisArray_FloraEarth"][:,2]*np.cos(obliquity_rad) + FloraEphemDict["ephemerisArray_FloraEarth"][:,3]*np.sin(obliquity_rad)
+FloraEarth_k = FloraEphemDict["ephemerisArray_FloraEarth"][:,2]*np.sin(obliquity_rad) + FloraEphemDict["ephemerisArray_FloraEarth"][:,3]*np.cos(obliquity_rad) 
 
 # Titan 
 TitanEphemDict = {
     'head': 'Titan',
     'tail':'Earth',
 }
-TitanEphemDict["ephemerisArray_TitanEarth"] = np.loadtxt("./Horizons New files/Titan_Earth.txt", delimiter=',',usecols=(0,2,3,4))
+TitanEphemDict["ephemerisArray_TitanEarth"] = np.loadtxt("./Titan_Earth.txt", delimiter=',',usecols=(0,2,3,4))
 # pull JD vectors 
 TitanEarth_JD = TitanEphemDict["ephemerisArray_TitanEarth"][:,0]
-# pull out i, j, k vecotrs
+# pull out i, j, k ; I is direct from X, but project Y and Z with obliquity to get J and K
 TitanEarth_i = TitanEphemDict["ephemerisArray_TitanEarth"][:,1]
-TitanEarth_j = TitanEphemDict["ephemerisArray_TitanEarth"][:,2]
-TitanEarth_k = TitanEphemDict["ephemerisArray_TitanEarth"][:,3]
+TitanEarth_j = TitanEphemDict["ephemerisArray_TitanEarth"][:,2]*np.cos(obliquity_rad) + TitanEphemDict["ephemerisArray_TitanEarth"][:,3]*np.sin(obliquity_rad)
+TitanEarth_k = TitanEphemDict["ephemerisArray_TitanEarth"][:,2]*np.sin(obliquity_rad) + TitanEphemDict["ephemerisArray_TitanEarth"][:,3]*np.cos(obliquity_rad) 
+#TitanEarth_j = TitanEphemDict["ephemerisArray_TitanEarth"][:,2]
+#TitanEarth_k = TitanEphemDict["ephemerisArray_TitanEarth"][:,3]
 
 # Charon 
 CharonEphemDict = {
     'head':'Charon',
     'tail':'Earth',
 }
-CharonEphemDict["ephemerisArray_CharonEarth"] = np.loadtxt("./Horizons New files/Charon_Earth.txt", delimiter=',',usecols=(0,2,3,4))
+CharonEphemDict["ephemerisArray_CharonEarth"] = np.loadtxt("./Charon_Earth.txt", delimiter=',',usecols=(0,2,3,4))
 # pull JD vectors 
 CharonEarth_JD = CharonEphemDict["ephemerisArray_CharonEarth"][:,0]
-# pull out i, j, k vecotrs
+# pull out i, j, k ; I is direct from X, but project Y and Z with obliquity to get J and K
 CharonEarth_i = CharonEphemDict["ephemerisArray_CharonEarth"][:,1]
-CharonEarth_j = CharonEphemDict["ephemerisArray_CharonEarth"][:,2]
-CharonEarth_k = CharonEphemDict["ephemerisArray_CharonEarth"][:,3]
+CharonEarth_j = CharonEphemDict["ephemerisArray_CharonEarth"][:,2]*np.cos(obliquity_rad) + CharonEphemDict["ephemerisArray_CharonEarth"][:,3]*np.sin(obliquity_rad)
+CharonEarth_k = CharonEphemDict["ephemerisArray_CharonEarth"][:,2]*np.sin(obliquity_rad) + CharonEphemDict["ephemerisArray_CharonEarth"][:,3]*np.cos(obliquity_rad) 
+# CharonEarth_j = CharonEphemDict["ephemerisArray_CharonEarth"][:,2]
+# CharonEarth_k = CharonEphemDict["ephemerisArray_CharonEarth"][:,3]
 
 #  Hippocamp
 HippocampEphemDict = {
     'head':'Hippocamp',
     'tail':'Earth',
 }
-HippocampEphemDict["ephemerisArray_HippocampEarth"] = np.loadtxt("./Horizons New files/Hippocamp_Earth.txt", delimiter=',',usecols=(0,2,3,4))
+HippocampEphemDict["ephemerisArray_HippocampEarth"] = np.loadtxt("./Hippocamp_Earth.txt", delimiter=',',usecols=(0,2,3,4))
 # pull JD vectors 
 HippocampEarth_JD = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,0]
-# pull out i, j, k vecotrs
+# pull out i, j, k ; I is direct from X, but project Y and Z with obliquity to get J and 
 HippocampEarth_i = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,1]
-HippocampEarth_j = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,2]
-HippocampEarth_k = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,3]
+HippocampEarth_j = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,2]*np.cos(obliquity_rad) + HippocampEphemDict["ephemerisArray_HippocampEarth"][:,3]*np.sin(obliquity_rad)
+HippocampEarth_k = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,2]*np.sin(obliquity_rad) + HippocampEphemDict["ephemerisArray_HippocampEarth"][:,3]*np.cos(obliquity_rad) 
+#HippocampEarth_j = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,2]
+#HippocampEarth_k = HippocampEphemDict["ephemerisArray_HippocampEarth"][:,3]
 
 ## convert from x,y,z position vector to RA and Dec
 # Dec is tan between x and z (output in rad-?)
-Dec_FloraEarth = np.arctan((FloraEarth_k)/(FloraEarth_i))
-Dec_CharonEarth = np.arctan((CharonEarth_k)/(CharonEarth_i))
-Dec_TitanEarth = np.arctan((TitanEarth_k)/(TitanEarth_i))
-Dec_HippocampEarth = np.arctan((HippocampEarth_k)/(HippocampEarth_i))
+FloraEarth_projIJ = np.sqrt((FloraEarth_i**2) + (FloraEarth_j**2))
+Dec_FloraEarth = np.arctan2(FloraEarth_k,FloraEarth_projIJ ) 
+# Dec_FloraEarth = np.arctan((FloraEarth_k)/(FloraEarth_i))
+
+CharonEarth_projIJ = np.sqrt((CharonEarth_i**2) + (CharonEarth_j**2))
+Dec_CharonEarth = np.arctan2(CharonEarth_k,CharonEarth_projIJ)
+
+# Dec_TitanEarth = np.arctan((TitanEarth_k)/(TitanEarth_i))
+TitanEarth_projIJ = np.sqrt((TitanEarth_i**2) + (TitanEarth_j**2))
+Dec_TitanEarth = np.arctan2(TitanEarth_k,TitanEarth_projIJ )
+
+HippocampEarth_projIJ = np.sqrt((HippocampEarth_i**2) + (HippocampEarth_j**2))
+Dec_HippocampEarth = np.arctan2(HippocampEarth_k,HippocampEarth_projIJ)
 ## check these ^^
 
 # RA is tan between x and y ( output in rad)
-RA_FloraEarth = np.arctan((FloraEarth_j)/(FloraEarth_i))
-RA_CharonEarth = np.arctan((CharonEarth_j)/(CharonEarth_i))
-Ra_TitanEarth = np.arctan((TitanEarth_j)/(TitanEarth_i))
-RA_HippocampEarth = np.arctan((HippocampEarth_j)/(HippocampEarth_i))
+# RA_FloraEarth = np.arctan2(FloraEarth_j*np.cos(obliquity_rad),FloraEarth_i)
+RA_FloraEarth = np.arctan2(FloraEarth_j,FloraEarth_i)
+RA_CharonEarth = np.arctan2(CharonEarth_j,CharonEarth_i)
+RA_TitanEarth = np.arctan2(TitanEarth_j,TitanEarth_i)
+RA_HippocampEarth = np.arctan2(HippocampEarth_j,HippocampEarth_i)
 
 # convert Dec to Deg 
-Dec_FloraEarth_deg = np.degrees(Dec_FloraEarth)
+Dec_FloraEarth_deg = np.degrees(Dec_FloraEarth) 
 Dec_CharonEarth_deg = np.degrees(Dec_CharonEarth)
 Dec_TitanEarth_deg = np.degrees(Dec_TitanEarth)
 Dec_HippocampEarth_deg = np.degrees(Dec_HippocampEarth)
@@ -87,4 +115,5 @@ RA_CharonEarth_deg = np.degrees(RA_CharonEarth)
 RA_TitanEarth_deg = np.degrees(RA_TitanEarth)
 RA_HippocampEarth_deg = np.degrees(RA_HippocampEarth)
 
+danDebugTestPoint = 42
 # covnert from deg to hr/min/sec in deg 
